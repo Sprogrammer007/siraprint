a1ea79f602be9f0c5e0826193f2cedab7a0baf92
Steve <stevenag006@hotmail.com>
2014-11-17 15:46:00 -0500

    test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,15 @@ class StaticPagesController < ApplicationController
   end
 
   def about
+    @no_sidebar = true
   end
 
   def help
+    @no_sidebar = true
   end
 
   def contact
+    @no_sidebar = true
   end
 
 end
