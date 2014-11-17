21655af17ed1ce534f40689020b8c1a827799fca
Steve <stevenag006@hotmail.com>
2014-11-16 21:26:01 -0500

    small changes

--- db/migrate/20141112040523_create_metal_sign_sizes.rb
+++ db/migrate/20141112040523_create_metal_sign_sizes.rb
@@ -2,8 +2,8 @@ class CreateMetalSignSizes < ActiveRecord::Migration
   def change
     create_table :metal_sign_sizes do |t|
       t.references :metal_sign
-      t.integer :width
-      t.integer :height
+      t.decimal :width
+      t.decimal :height
       t.string :unit
       t.decimal :price
 
