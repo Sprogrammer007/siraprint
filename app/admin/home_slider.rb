6403bffd8189b0c51cd09305caa6b6622f84b71a
Steve <stevenag006@hotmail.com>
2015-01-11 18:46:05 -0500

    test

--- app/admin/home_slider.rb
+++ app/admin/home_slider.rb
@@ -1,16 +1,16 @@
-  ActiveAdmin.register_page "Home Slider Images" do
-    menu :parent => "Global"
+ActiveAdmin.register_page "Home Slider Images" do
+  menu :parent => "Global"
 
-    content do
-      div class: "slides" do
-        if SliderImage.home.any?
-          SliderImage.home.each do |image|
-            render "slider_images/slider_image", slider_image: image
-          end
+  content do
+    div class: "slides" do
+      if SliderImage.home.any?
+        SliderImage.home.each do |image|
+          render "slider_images/slider_image", slider_image: image
         end
       end
-      render "admin/upload_slider", type: "home"
-
     end
+    render "admin/upload_slider", type: "home"
+
   end
+end
 
