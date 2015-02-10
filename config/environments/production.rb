20612c89bb1d9fec0cb4ba5b64d926524aff6542
Steve <stevenag006@hotmail.com>
2015-02-09 23:24:11 -0500

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
+    domain: "siraprint.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "steve00006@gmail.com",
