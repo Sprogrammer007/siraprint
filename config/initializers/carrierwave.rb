fd3a337083d730b35a0cf37e63214878bbf2ff3a
Steve <stevenag006@hotmail.com>
2015-05-04 12:12:09 -0400

    s3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |config|
 
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'us-west-2'
   }
 end
