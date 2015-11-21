b9f878ab073bcc0585f475df7e9ef8ec942130f6
Steve <stevenag006@hotmail.com>
2015-11-21 18:57:47 -0500

    fix upload slider iamge

--- app/models/slider_image.rb
+++ app/models/slider_image.rb
@@ -7,4 +7,13 @@ class SliderImage < ActiveRecord::Base
 
   mount_uploader :slide_image_file_name, SliderImageUploader
   validates_presence_of :slide_image_file_name 
+  # Prevent modification of existing records
+  def readonly?
+     !new_record?
+  end
+
+  # Prevent objects from being destroyed
+  def before_destroy
+    raise ActiveRecord::ReadOnlyRecord
+  end
 end
